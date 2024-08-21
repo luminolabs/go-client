8b9d7dd454bb5ce8bdbdac29bd139fea53032622
Shyam Patel <shyampkira@gmail.com>
2024-08-21 14:23:27 -0700

    Add comments for root.go

--- cmd/root.go
+++ cmd/root.go
@@ -10,14 +10,18 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// log is the package-level logger instance
 var log = logger.NewLogger()
 
+// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:     "luminocli",
 	Short:   "Lumino CLI is a command line interface for interacting with the Lumino network",
 	Version: core.Version,
 }
 
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
@@ -25,11 +29,8 @@ func Execute() {
 	}
 }
 
+// init initializes the root command by setting up persistent flags
 func init() {
 	rootCmd.PersistentFlags().StringP("config", "c", "", "config file (default is $HOME/.luminocli.yaml)")
 	rootCmd.PersistentFlags().StringP("rpc-url", "r", "", "RPC URL of the Ethereum node")
-}
-
-func main() {
-	Execute()
 }
