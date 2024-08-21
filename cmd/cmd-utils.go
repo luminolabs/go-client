21b558af5a15c7e41f77da5fbd0c52a76dc18416
Shyam Patel <shyampkira@gmail.com>
2024-08-20 19:19:36 -0700

    Setup config utils

---
+++ cmd/cmd-utils.go
@@ -0,0 +1,53 @@
+// generate file yet to be modified
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/spf13/cobra"
+)
+
+// confirmAction asks for user confirmation before proceeding
+func confirmAction(prompt string) bool {
+	fmt.Printf("%s (y/n): ", prompt)
+	var response string
+	_, err := fmt.Scanln(&response)
+	if err != nil {
+		return false
+	}
+	return response == "y" || response == "Y"
+}
+
+// handleError prints the error and exits the program
+func handleError(err error) {
+	fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+	os.Exit(1)
+}
+
+// addCommonFlags adds flags that are used across multiple commands
+func addCommonFlags(cmd *cobra.Command) {
+	cmd.Flags().StringP("rpc-url", "r", "", "RPC URL of the Ethereum node")
+	cmd.Flags().StringP("private-key", "k", "", "Private key for transaction signing")
+}
+
+// This function takes client as a parameter and returns the epoch and state
+func (*UtilsStruct) GetEpochAndState(client *ethclient.Client) (uint32, int64, error) {
+	epoch, err := protoUtils.GetEpoch(client)
+	if err != nil {
+		return 0, 0, err
+	}
+	bufferPercent, err := cmdUtils.GetBufferPercent()
+	if err != nil {
+		return 0, 0, err
+	}
+	state, err := protoUtils.GetDelayedState(client, bufferPercent)
+	if err != nil {
+		return 0, 0, err
+	}
+	log.Debug("Epoch ", epoch)
+	log.Debug("State ", utils.GetStateName(state))
+	return epoch, state, nil
+}
