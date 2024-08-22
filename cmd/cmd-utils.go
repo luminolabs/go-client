53208371507e1be49df3f7928b07e69ae3e8c12f
Shyam Patel <shyampkira@gmail.com>
2024-08-21 17:28:39 -0700

    clean up

--- cmd/cmd-utils.go
+++ cmd/cmd-utils.go
@@ -5,25 +5,12 @@ package cmd
 import (
 	"fmt"
 	"os"
-	"strconv"
 
 	"lumino/utils"
 
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/spf13/cobra"
 )
-
-// confirmAction asks for user confirmation before proceeding with an action.
-// It returns true if the user confirms, false otherwise.
-func confirmAction(prompt string) bool {
-	fmt.Printf("%s (y/n): ", prompt)
-	var response string
-	_, err := fmt.Scanln(&response)
-	if err != nil {
-		return false
-	}
-	return response == "y" || response == "Y"
-}
 
 // handleError prints the error and exits the program
 func handleError(err error) {
@@ -55,18 +42,4 @@ func (*UtilsStruct) GetEpochAndState(client *ethclient.Client) (uint32, int64, e
 	log.Debug("Epoch ", epoch)
 	log.Debug("State ", utils.UtilsInterface.GetStateName(state))
 	return epoch, state, nil
-}
-
-// GetStatesAllowed returns a string representation of allowed states.
-// It takes a slice of int representing allowed states and returns a formatted string.
-func GetStatesAllowed(states []int) string {
-	var statesAllowed string
-	for i := 0; i < len(states); i++ {
-		if i == len(states)-1 {
-			statesAllowed = statesAllowed + strconv.Itoa(states[i]) + ":" + utils.UtilsInterface.GetStateName(int64(states[i]))
-		} else {
-			statesAllowed = statesAllowed + strconv.Itoa(states[i]) + ":" + utils.UtilsInterface.GetStateName(int64(states[i])) + ", "
-		}
-	}
-	return statesAllowed
 }
