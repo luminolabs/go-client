80fcd1d44ce5cf078c04753aded50d901f073e3f
Shyam Patel <shyampkira@gmail.com>
2024-08-28 16:00:01 -0700

    Update root.go and main.go

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"lumino/cmd"
 )
 
 func main() {
+	fmt.Println("Initializing interfaces")
 	cmd.InitializeInterfaces()
+	fmt.Println("Executing command")
 	cmd.Execute()
 }
