17cba2a6d79b9e7ad776c2e407ac5b624ed7ca9c
Shyam Patel <shyampkira@gmail.com>
2024-08-28 16:17:37 -0700

    Implement config utils

--- cmd/cfg-utils.go
+++ cmd/cfg-utils.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"lumino/core"
 	"lumino/core/types"
+	"lumino/utils"
 	"strings"
 
 	"github.com/spf13/viper"
@@ -42,12 +43,43 @@ func (*UtilsStruct) GetConfigData() (types.Configurations, error) {
 	if err != nil {
 		return config, err
 	}
+	gasMultiplier, err := cmdUtils.GetMultiplier()
+	if err != nil {
+		return config, err
+	}
 	bufferPercent, err := cmdUtils.GetBufferPercent()
 	if err != nil {
 		return config, err
 	}
+	waitTime, err := cmdUtils.GetWaitTime()
+	if err != nil {
+		return config, err
+	}
+	gasPrice, err := cmdUtils.GetGasPrice()
+	if err != nil {
+		return config, err
+	}
+	logLevel, err := cmdUtils.GetLogLevel()
+	if err != nil {
+		return config, err
+	}
+	gasLimit, err := cmdUtils.GetGasLimit()
+	if err != nil {
+		return config, err
+	}
+	rpcTimeout, err := cmdUtils.GetRPCTimeout()
+	if err != nil {
+		return config, err
+	}
 	config.Provider = provider
+	config.GasMultiplier = gasMultiplier
 	config.BufferPercent = bufferPercent
+	config.WaitTime = waitTime
+	config.GasPrice = gasPrice
+	config.LogLevel = logLevel
+	config.GasLimitMultiplier = gasLimit
+	config.RPCTimeout = rpcTimeout
+	utils.RPCTimeout = rpcTimeout
 
 	return config, nil
 }
@@ -70,4 +102,107 @@ func (*UtilsStruct) GetRPCProvider() (string, error) {
 		log.Warn("You are not using a secure RPC URL. Switch to an https URL instead to be safe.")
 	}
 	return provider, nil
+}
+
+// This function returns the multiplier
+func (*UtilsStruct) GetMultiplier() (float32, error) {
+	gasMultiplier, err := flagSetUtils.GetRootFloat32GasMultiplier()
+	if err != nil {
+		return float32(core.DefaultGasMultiplier), err
+	}
+	if gasMultiplier == -1 {
+		if viper.IsSet("gasmultiplier") {
+			gasMultiplier = float32(viper.GetFloat64("gasmultiplier"))
+		} else {
+			gasMultiplier = float32(core.DefaultGasMultiplier)
+			log.Debug("GasMultiplier is not set, taking its default value ", gasMultiplier)
+		}
+	}
+	return gasMultiplier, nil
+}
+
+// This function returns the wait time
+func (*UtilsStruct) GetWaitTime() (int32, error) {
+	waitTime, err := flagSetUtils.GetRootInt32Wait()
+	if err != nil {
+		return int32(core.DefaultWaitTime), err
+	}
+	if waitTime == -1 {
+		if viper.IsSet("wait") {
+			waitTime = viper.GetInt32("wait")
+		} else {
+			waitTime = int32(core.DefaultWaitTime)
+			log.Debug("WaitTime is not set, taking its default value ", waitTime)
+		}
+	}
+	return waitTime, nil
+}
+
+// This function returns the gas price
+func (*UtilsStruct) GetGasPrice() (int32, error) {
+	gasPrice, err := flagSetUtils.GetRootInt32GasPrice()
+	if err != nil {
+		return int32(core.DefaultGasPrice), err
+	}
+	if gasPrice == -1 {
+		if viper.IsSet("gasprice") {
+			gasPrice = viper.GetInt32("gasprice")
+		} else {
+			gasPrice = int32(core.DefaultGasPrice)
+			log.Debug("GasPrice is not set, taking its default value ", gasPrice)
+
+		}
+	}
+	return gasPrice, nil
+}
+
+// This function returns the log level
+func (*UtilsStruct) GetLogLevel() (string, error) {
+	logLevel, err := flagSetUtils.GetRootStringLogLevel()
+	if err != nil {
+		return core.DefaultLogLevel, err
+	}
+	if logLevel == "" {
+		if viper.IsSet("logLevel") {
+			logLevel = viper.GetString("logLevel")
+		} else {
+			logLevel = core.DefaultLogLevel
+			log.Debug("LogLevel is not set, taking its default value ", logLevel)
+		}
+	}
+	return logLevel, nil
+}
+
+// This function returns the gas limit
+func (*UtilsStruct) GetGasLimit() (float32, error) {
+	gasLimit, err := flagSetUtils.GetRootFloat32GasLimit()
+	if err != nil {
+		return float32(core.DefaultGasLimit), err
+	}
+	if gasLimit == -1 {
+		if viper.IsSet("gasLimit") {
+			gasLimit = float32(viper.GetFloat64("gasLimit"))
+		} else {
+			gasLimit = float32(core.DefaultGasLimit)
+			log.Debug("GasLimit is not set, taking its default value ", gasLimit)
+		}
+	}
+	return gasLimit, nil
+}
+
+// This function returns the RPC timeout
+func (*UtilsStruct) GetRPCTimeout() (int64, error) {
+	rpcTimeout, err := flagSetUtils.GetRootInt64RPCTimeout()
+	if err != nil {
+		return int64(core.DefaultRPCTimeout), err
+	}
+	if rpcTimeout == 0 {
+		if viper.IsSet("rpcTimeout") {
+			rpcTimeout = viper.GetInt64("rpcTimeout")
+		} else {
+			rpcTimeout = int64(core.DefaultRPCTimeout)
+			log.Debug("RPCTimeout is not set, taking its default value ", rpcTimeout)
+		}
+	}
+	return rpcTimeout, nil
 }
