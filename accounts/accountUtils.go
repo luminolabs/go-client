946d52691b4660fb1a2c266d8fa5daf98e8cacbb
kira <shyampkira@gmail.com>
2024-09-09 11:50:39 -0700

    LUM-627: KeyManager (Accounts) package (#13)
    * Update Mocks
    
    * Update state.go to utils
    
    * Update default rpc; add stateManager Address
    
    * Update NetworkInfo function to use RPC; add networkInfo test file
    
    * Add networkInfo command related tests
    
    * Add mocks for keyManager
    
    * add accounts type to core package
    
    * add create account and helper functions
    
    * Add account utils and interface
    
    * Add Mocks for Accounts
    
    * Add tests for keyMaanger
    
    * Update go.mod
    
    * implemented create wallet cmd
    
    * Update addresses

---
+++ accounts/accountUtils.go
@@ -0,0 +1,58 @@
+// Package account provides all account related functions
+package accounts
+
+import (
+	"crypto/ecdsa"
+	"lumino/core/types"
+	"os"
+
+	"github.com/ethereum/go-ethereum/accounts"
+	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+//go:generate mockery --name AccountInterface --output ./mocks/ --case=underscore
+
+var AccountUtilsInterface AccountInterface
+
+type AccountInterface interface {
+	CreateAccount(path string, password string) accounts.Account
+	GetPrivateKeyFromKeystore(keystorePath string, password string) (*ecdsa.PrivateKey, error)
+	GetPrivateKey(address string, password string, keystorePath string) (*ecdsa.PrivateKey, error)
+	SignData(hash []byte, account types.Account, defaultPath string) ([]byte, error)
+	Accounts(path string) []accounts.Account
+	NewAccount(path string, passphrase string) (accounts.Account, error)
+	DecryptKey(jsonBytes []byte, password string) (*keystore.Key, error)
+	Sign(digestHash []byte, prv *ecdsa.PrivateKey) ([]byte, error)
+	ReadFile(filename string) ([]byte, error)
+}
+
+type AccountUtils struct{}
+
+// This function returns all the accounts in form of array
+func (accountUtils AccountUtils) Accounts(path string) []accounts.Account {
+	ks := keystore.NewKeyStore(path, keystore.StandardScryptN, keystore.StandardScryptP)
+	return ks.Accounts()
+}
+
+// This function takes path and pass phrase as input and returns the new account
+func (accountUtils AccountUtils) NewAccount(path string, passphrase string) (accounts.Account, error) {
+	ks := keystore.NewKeyStore(path, keystore.StandardScryptN, keystore.StandardScryptP)
+	accounts.NewManager(&accounts.Config{InsecureUnlockAllowed: false}, ks)
+	return ks.NewAccount(passphrase)
+}
+
+// This function takes json bytes array and password as input and returns the decrypted key
+func (accountUtils AccountUtils) DecryptKey(jsonBytes []byte, password string) (*keystore.Key, error) {
+	return keystore.DecryptKey(jsonBytes, password)
+}
+
+// This function takes hash in form of byte array and private key as input and returns signature as byte array
+func (accountUtils AccountUtils) Sign(digestHash []byte, prv *ecdsa.PrivateKey) (sig []byte, err error) {
+	return crypto.Sign(digestHash, prv)
+}
+
+// This function takes name of the file as input and returns the file data as byte array
+func (accountUtils AccountUtils) ReadFile(filename string) ([]byte, error) {
+	return os.ReadFile(filename)
+}
---
+++ cmd/create.go
@@ -0,0 +1,64 @@
+// Package cmd provides all functions related to command line
+package cmd
+
+import (
+	luminoAccounts "lumino/accounts"
+	"lumino/utils"
+	"path"
+
+	"github.com/ethereum/go-ethereum/accounts"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+)
+
+var createCmd = &cobra.Command{
+	Use:   "create",
+	Short: "create command can be used to create new accounts",
+	Long: `For a new user to start doing anything, an account is required. This command helps the user to create a new account secured by a password so that only that user would be able to use the account
+
+Example: 
+  ./lumino create --logFile createLogs`,
+	Run: initialiseCreate,
+}
+
+// This function initialises the ExecuteCreate function
+func initialiseCreate(cmd *cobra.Command, args []string) {
+	cmdUtils.ExecuteCreate(cmd.Flags())
+}
+
+// This function sets the flags appropriately and executes the Create function
+func (*UtilsStruct) ExecuteCreate(flagSet *pflag.FlagSet) {
+	log.Debug("Checking to assign log file...")
+	protoUtils.AssignLogFile(flagSet)
+	log.Info("The password should be of minimum 8 characters containing least 1 uppercase, lowercase, digit and special character.")
+	password := protoUtils.AssignPassword(flagSet)
+	log.Debug("ExecuteCreate: Calling Create() with argument as input password")
+	account, err := cmdUtils.Create(password)
+	utils.CheckError("Create error: ", err)
+	log.Info("ExecuteCreate: Account address: ", account.Address)
+	log.Info("ExecuteCreate: Keystore Path: ", account.URL)
+}
+
+// This function is used to create the new account
+func (*UtilsStruct) Create(password string) (accounts.Account, error) {
+	luminoPath, err := protoUtils.GetDefaultPath()
+	if err != nil {
+		log.Error("Error in fetching .lumino directory")
+		return accounts.Account{Address: common.Address{0x00}}, err
+	}
+	log.Debug("Create: .lumino directory path: ", luminoPath)
+	keystorePath := path.Join(luminoPath, "keystore_files")
+	account := luminoAccounts.AccountUtilsInterface.CreateAccount(keystorePath, password)
+	return account, nil
+}
+
+func init() {
+	rootCmd.AddCommand(createCmd)
+
+	var (
+		Password string
+	)
+
+	createCmd.Flags().StringVarP(&Password, "password", "", "", "password file path to protect the keystore")
+}
