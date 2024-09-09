a893dda9eafd220df7348fa3709729fd64acd4c8
Shyam Patel <shyampkira@gmail.com>
2024-09-09 11:23:14 -0700

    Update addresses

--- core/contracts.go
+++ core/contracts.go
@@ -1,7 +1,7 @@
 package core
 
-var StateManagerAddress = ""
-var StakeManagerAddress = ""
+var StateManagerAddress = "0x0a2946DaECb5f2A693e3f814612eb0BC01dC5Fd5"
+var StakeManagerAddress = "0x3c24e5485a38173f79eb7ed20517dea4b6cbf5e5"
 var JobManagerAddress = ""
 var VoteManagerAddress = ""
 var BlockManagerAddress = ""
