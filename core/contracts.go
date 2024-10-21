12085ef1e93175872a32ed84789f7ea11aadd5fe
Shyam Patel <shyampkira@gmail.com>
2024-10-21 15:13:13 -0700

    Update generate binding script to generate bindings for jobManager

--- core/contracts.go
+++ core/contracts.go
@@ -2,6 +2,6 @@ package core
 
 var StateManagerAddress = "0x0a2946DaECb5f2A693e3f814612eb0BC01dC5Fd5"
 var StakeManagerAddress = "0x3c24e5485a38173f79eb7ed20517dea4b6cbf5e5"
-var JobManagerAddress = ""
+var JobManagerAddress = "0xCA1281ddcB9553A483f4c6C25067Abfb5E3C61Cb"
 var VoteManagerAddress = ""
 var BlockManagerAddress = ""
